35690e5f9bd421de15b649549ce85cdac53bc399
MISHANDLED <52106838+MISHANDLED@users.noreply.github.com>
2020-04-13 21:05:28 +0530

    Update Last_Stone_Weight.py
    Optimized by removing the unnecessary if else statements.

--- Problems/Last_Stone_Weight.py
+++ Problems/Last_Stone_Weight.py
@@ -3,25 +3,16 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         res = stones[0]
+        
         while len(stones) > 2:
-            res = stones[0]
+            
             x = max(stones)
             stones.remove(x)
             y = max(stones)
             stones.remove(y)
-            if x > y:
-                stones.append(x-y)
-            elif x < y:
-                stones.append(y-x)
-            else:
-                stones.append(0)
+            stones.append(abs(x-y))
+        
         if len(stones) == 2:
-            x = stones[0]
-            y = stones[-1]
-            if x > y:
-                return x - y
-            elif x < y:
-                return y - x
-            else:
-                return 0
+            return abs(stones[0] - stones[1])
+
         return res
