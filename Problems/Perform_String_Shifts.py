15ebfd31a92b671d6f4bb0fa098bc250a652dcd7
Raman Gandhi <ramangandhi392000@gmail.com>
2020-04-14 17:02:40 +0530

    Update Perform_String_Shifts.py

--- Problems/Perform_String_Shifts.py
+++ Problems/Perform_String_Shifts.py
@@ -1,12 +1,19 @@
-# Runtime : 40 ms
+# Runtime : 36 ms
 # Memory Usage : 13.9 MB
 class Solution:
     def stringShift(self, s: str, shift: List[List[int]]) -> str:
         n = len(shift)
-        for i in range(n):
-            for j in range(shift[i][1]):
-                if shift[i][0] == 0:
-                    s = s[1:] + s[0]
-                elif shift[i][0] == 1:
-                    s =  s[-1] + s[:len(s)-1]
+        count = 0
+
+        for x in range(n):
+            if shift[x][0] == 0:
+                count -= shift[x][1]
+            else:
+                count += shift[x][1]
+
+        for j in range(abs(count)):
+            if count < 0:
+                s = s[1:] + s[0]
+            else:
+                s =  s[-1] + s[:len(s)-1]
         return s
