d7f89e805100ff9e49c704e7fc48283199a41f79
Raman Gandhi <ramangandhi392000@gmail.com>
2020-04-20 11:34:57 +0530

    Create Search_In_Rotated_Sorted_Array.py

---
+++ Problems/Search_In_Rotated_Sorted_Array.py
@@ -0,0 +1,8 @@
+# Runtime : 48 ms
+# Memory Usage : 14 MB
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+        else:
+            return -1
